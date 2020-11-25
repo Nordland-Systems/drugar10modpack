ebcc10187a9a678372a18a11f956463a7963f84f
Steffen Kahl <steffen.kahl@ue-germany.de>
2020-11-25 03:01:43 +0100

    [CHANGED] Way to create ZIP of files

---
+++ ZipAllFiles.bat
@@ -0,0 +1,16 @@
+set REL_PATH=.\
+set ABS_PATH=
+
+rem // Save current directory and change to target directory
+pushd %REL_PATH%
+
+rem // Save value of CD variable (current directory)
+set ABS_PATH=%CD%
+
+rem // Restore original directory
+popd
+
+echo Relative path: %REL_PATH%
+echo Maps to path: %ABS_PATH%
+
+CScript  zip.vbs  %ABS_PATH%\drugar10  %ABS_PATH%\drugar10Mods.zip
